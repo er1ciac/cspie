2da671c44e4bc5bd9adf0d1280ff2d3d07ccd784
er1ciac <yhp3297341132@gmail.com>
2025-07-12 15:23:59 +0800

    add main func for baseline

---
+++ project4/main.cpp
@@ -0,0 +1,29 @@
+#include "sm3.h"
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main()
+{
+    const char *msg = "sm3 example message";
+    uint32_t digest[8];
+    uint8_t hash[32];
+    {
+        Timer t("SM3 start");
+        for (int i = 0; i < 100; ++i)
+        {
+            sm3_init(digest);
+            sm3_update(digest, (const uint8_t *)msg, strlen(msg));
+            sm3_final(digest, (const uint8_t *)msg, strlen(msg), hash);
+        }
+    }
+
+    std::cout << "SM3(\"" << msg << "\") = ";
+    for (int i = 0; i < 32; ++i)
+    {
+        printf("%02x", hash[i]);
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
